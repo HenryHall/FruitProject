ac157505e8d5e3bfd90094d7d5b97eb88ff47ef4
HenryHall <HenryHall@Henrys-MacBook-Pro.local>
2016-06-01 17:03:49 -0500

    second commit

--- script.js
+++ script.js
@@ -33,10 +33,6 @@ $(document).ready(function() {
   $('.buyFruit').click(function() {
 
     fruitType = this.value;
-    // fruitOwned = userObject.fruit[fruitType][0];
-    // fruitPrice = venderObject[fruitType];
-    // fruitBought = userObject.fruit[fruitType][1];
-    // userMoney = userObject.money;
 
     if ( userObject.money < venderObject[fruitType]) {
       alert("Get a job!");
@@ -47,10 +43,45 @@ $(document).ready(function() {
       userObject.fruit[fruitType][1].push(venderObject[fruitType]);
       console.log(userObject.fruit[fruitType][1]);
       userObject.money-=venderObject[fruitType];
+
+      var total = 0;
+      for(var i = 0; i < userObject.fruit[fruitType][1].length; i++) {
+        total += userObject.fruit[fruitType][1][i];
+      }
+      var fruitAvg = total / userObject.fruit[fruitType][1].length;
+
+      $('#userMoney').text('$' + userObject.money.toFixed(2));
+      $('#' + fruitType + 'Amount').text(userObject.fruit[fruitType][0]);
+      $('#' + fruitType + 'Avg').text(fruitAvg.toFixed(2));
       console.log(userObject.money);
+      console.log(fruitType + ' has been purchased, you now have ' + userObject.fruit[fruitType][0] +
+       ' which have cost you ' + userObject.fruit[fruitType][1] + '. You now have ' + userObject.money + ' left!' );
     }
-    console.log(fruitType + ' has been purchased, you now have ' + userObject.fruit[fruitType][0] +
-     ' which have cost you ' + userObject.fruit[fruitType][1] + '. You now have ' + userObject.money + ' left!' );
+
+  });
+
+  $('.sellFruit').click(function() {
+
+    fruitType = this.value;
+
+    if ( userObject.fruit[fruitType][0] < 1) {
+      alert("You don't have any " + fruitType + " to sell!");
+      return;
+    } else {
+      userObject.fruit[fruitType][0]--;
+      console.log(userObject.fruit[fruitType][0]);
+      userObject.fruit[fruitType][2].push(venderObject[fruitType]);
+      console.log(userObject.fruit[fruitType][2]);
+      userObject.money+=venderObject[fruitType];
+
+      $('#userMoney').text('$' + userObject.money.toFixed(2));
+      $('#' + fruitType + 'Amount').text(userObject.fruit[fruitType][0]);
+
+      console.log(userObject.money);
+      console.log(fruitType + ' has been sold, you now have ' + userObject.fruit[fruitType][0] +
+       ' which have netted you ' + userObject.fruit[fruitType][2] + '. You now have ' + userObject.money + ' left!' );
+    }
+
   });
 
 });
